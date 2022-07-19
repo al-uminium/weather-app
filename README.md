0683277abad6fb5faa4983aba5e8d695b3af2b3c
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-07-19 15:00:40 +0800

    added in background photos

---
+++ README.md
@@ -0,0 +1,8 @@
+# Revisiting weather-app 
+This is a revisit of the weather app from The Odin Project :)
+
+## Problems faced:
+- There are several towns/cities that share the same name (e.g., London is being shared by UK, US and CA), so by querying "London" would not be accurate.
+
+## Learning points:
+- Learnt how to change background image dynamically (based on weather condition)
---
+++ index.js
@@ -0,0 +1,13 @@
+import { APIKEY } from './apikey.js' //file was .gitignored
+
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${APIKEY}`, {mode: 'cors'})
+    .then((response) => {
+        console.log(response.json())
+})
+
+let cityQuery = "Punkeydoodles Corners"
+
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${APIKEY}`, {mode: 'cors'})
+    .then((response) => {
+        console.log(response.json())
+})
