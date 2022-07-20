2669e52f9431482fab53b6dd38e0b2887e0a2d6e
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-07-20 16:28:35 +0800

    added helper function to convert api response to something more accessible

--- main.js
+++ main.js
@@ -5,6 +5,57 @@ import { APIKEY } from './apikey.js' //file was .gitignored
 //         console.log(response.json())
 // })
 
+const getWeatherData = async() => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=${APIKEY}`, {mode: 'cors'})
+    const data = await response.json()
+    return data
+}
+
+const getLatAndLon = async() => {
+
+}
+
+const convertWeatherData = async() => {
+    const data = await getWeatherData()
+    
+    //FL -> Feels Like
+    // TODO: generate weatherData dynamically
+    const weatherData = {
+        currentTemp: null,
+        currentFeelsLike: null,
+        currentWeather: null,
+        currentWeatherID: null,
+    }
+    
+    // to add hourly weather dynamically
+    for (let i = 0; i < 4; i++){
+        weatherData[`hourlyTemp${i+1}`] = null;
+        weatherData[`hourlyFeelsLike${i+1}`] = null;
+        weatherData[`hourlyWeather${i+1}`] = null;
+        weatherData[`hourlyWeatherID${i+1}`] = null;
+    }
+
+    //amend current weather data
+    weatherData.currentTemp = data.current.temp;
+    weatherData.currentFeelsLike = data.current.feels_like;
+    weatherData.currentWeather = data.current.weather[0].main
+    weatherData.currentWeatherID = data.current.weather[0].id
+
+    //amend hourly weather data
+    for (let i = 0; i < 4; i++) {
+        weatherData[`hourlyTemp${i+1}`] = data.hourly[i].temp;
+        weatherData[`hourlyFeelsLike${i+1}`] = data.hourly[i].feels_like;
+        weatherData[`hourlyWeather${i+1}`] = data.hourly[i].weather[0].main;
+        weatherData[`hourlyWeatherID${i+1}`] = data.hourly[i].weather[0].id;
+    }
+
+    console.log(weatherData)
+
+    return weatherData
+}
+
+let x = convertWeatherData()
+
 // let cityQuery = "Punkeydoodles Corners"
 
 // fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${APIKEY}`, {mode: 'cors'})
