6c13a17ee8d0c0c42269eacca50234ff8f4cdff4
al-uminium <aeronafic@gmail.com>
2022-09-04 19:51:22 +0800

    added searchbar functionality

--- main.js
+++ main.js
@@ -23,13 +23,14 @@ const getLatAndLon = async(cityName, stateCode=undefined, countryCode=undefined)
 
 const convertCityToLatLon = async (cityName, stateCode, countryCode) => {
     const data = await getLatAndLon(cityName, stateCode, countryCode)
-    console.log("city to latlon")
-    console.log(data[0].lat)
-    console.log(data[0].lon)
-    const lat = data[0].lat;
-    const lon = data[0].lon;
+    if (data.length) {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        return {lat, lon};
+    } else {
+        return null
+    }
 
-    return {lat, lon};
 }
 
 const convertWeatherData = async(lat, lon) => {
@@ -94,6 +95,24 @@ const DisplayController = (() => {
     const currentTemp = document.querySelector(".current-temp");
     const currentIcon = document.querySelector(".current-icon");
     const feelsLike = document.querySelector(".feels-like");
+    const currentWeatherContainer = document.querySelector(".currweather-container");
+    const forecastContainer = document.querySelector(".hourly-forecast-container")
+
+    const changeToDark = () => {
+        const displays = [forecastContainer, umbrellaStatus, currentWeatherContainer]
+        
+        displays.forEach(ctn => {
+            ctn.classList.add("dark-font")
+        });
+    }
+
+    const changeToLight = () => {
+        const displays = [forecastContainer, umbrellaStatus, currentWeatherContainer]
+
+        displays.forEach(ctn => {
+            ctn.classList.add("light-font")
+        });
+    }
 
     const updateCurrentWeatherAndUmbrellaStatus = (currentWeatherStatus) => {
         // TODO - add in night variants for background.  
@@ -102,76 +121,91 @@ const DisplayController = (() => {
                 weatherStatus.innerText = "It's storming out there."
                 umbrellaStatus.innerText = "You definitely need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/thunderstorm.jpg')"
+                changeToLight()
                 break
             case "Drizzle":
                 weatherStatus.innerText = "It's drizzling."
                 umbrellaStatus.innerText = "You need an umbrella ☂️"
                 document.body.style.backgroundImage = "url('./background-img/drizzle.jpg')"
+                changeToLight()
                 break
             case "Rain":
                 weatherStatus.innerText = "It's raining!"
                 umbrellaStatus.innerText = "You need an umbrella ☔️"
                 document.body.style.backgroundImage = "url('./background-img/rain.jpg')"
+                changeToLight()
                 break
             case "Snow":
                 weatherStatus.innerText = "It's snowing!"
                 umbrellaStatus.innerHTML = "You need an umbrella, and a jacket to keep warm! 🌨"
                 document.body.style.backgroundImage = "url('./background-img/snow.jpg')"
+                changeToDark()
                 break
             case "Mist":
                 weatherStatus.innerText = "It's misty."
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_mist.jpg')"
+                changeToDark()
                 break
             case "Smoke":
                 weatherStatus.innerText = "The air's smoky out there"
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_Smoke.jpg')"
+                changeToDark()
                 break
             case "Haze":
                 weatherStatus.innerText = "It's kinda hazy on the horizon..."
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_haze.jpg')"
+                changeToDark()
                 break
             case "Dust":
                 weatherStatus.innerText = "The winds are whipping up the dust!"
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_dust.jpg')"
+                changeToLight()
                 break
             case "Fog":
                 weatherStatus.innerText = "Fog is encroaching the area"
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_fog.jpg')"
+                changeToDark()
                 break
             case "Sand":
                 weatherStatus.innerText = "The winds are whipping up the sand!"
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_dust.jpg')"
+                changeToLight()
                 break
             case "Ash":
                 weatherStatus.innerText = "Ashes from the mountain is trickling down"
                 umbrellaStatus.innerText = "An umbrella might help."
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_ash.jpg')"
+                changeToLight()
                 break
             case "Squall":
                 weatherStatus.innerText = "There's a squall in the area!"
                 umbrellaStatus.innerText = "You need an umbrella, if you're heading out!"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_squall.jpg')"
+                changeToDark()
                 break
             case "Tornado":
                 weatherStatus.innerText = "There's a torndao in the area!"
                 umbrellaStatus.innerText = "You need an umbrella, if you're heading out!"
                 document.body.style.backgroundImage = "url('./background-img/atmosphere_tornado.jpg')"
+                changeToLight()
                 break
             case "Clear":
                 weatherStatus.innerText = "The skies are clear."
                 umbrellaStatus.innerText = "You don't need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/clear.jpg')"
+                changeToDark()
                 break
             case "Clouds":
                 weatherStatus.innerText = "The skies are cloudy."
                 umbrellaStatus.innerText = "You might need an umbrella"
                 document.body.style.backgroundImage = "url('./background-img/clouds.jpg')"
+                changeToDark()
                 break
         }
     }
@@ -202,28 +236,34 @@ const DisplayController = (() => {
 
     const updateHourlyTemps = (hourlyTemp) => {
         for (let i = 0; i<4; i++) {
-            hourlyTemps[i].innerText = hourlyTemp[i]
+            hourlyTemps[i].innerText = `${hourlyTemp[i]}°C`
         }
     }
     
     const updateHourlyFeelsLike = (hourlyFeelsLike) => {
         for (let i = 0; i < 4; i++) {
-            hourlyFeelsLikes[i].innerText = "feels like " + hourlyFeelsLike[i]
+            hourlyFeelsLikes[i].innerText = `feels like ${hourlyFeelsLike[i]}°C`
         }
     }
 
     const errorMessage = () => {
         umbrellaStatus.innerText = "This city doesn't exist :(";
-        const currWeatherCtn = document.querySelector(".currweather-container");
-        const hrlyForecastCtn = document.querySelector(".hourly-forecast-container");
 
-        const containers = [currWeatherCtn, hrlyForecastCtn]
+        const containers = [currentWeatherContainer, forecastContainer]
 
         containers.forEach(ctn => {
-            console.log(ctn)
-            ctn.style.display = "none"
+            ctn.style.opacity = "0"
         });
     }
+
+    const displayIfNoError = () => {
+        const containers = [currentWeatherContainer, forecastContainer]
+
+        containers.forEach(ctn => {
+            ctn.style.opacity = "100"
+        });
+    }
+
     return {
         updateCurrentWeatherAndUmbrellaStatus, 
         updateCurrentWeatherIcon,
@@ -233,8 +273,24 @@ const DisplayController = (() => {
         updateHourlyTemps,
         updateHourlyFeelsLike,
         errorMessage,
+        displayIfNoError,
     }
 })()
+
+const searchBar = document.querySelector(".searchbar");
+
+searchBar.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        //Check if city exists first before running through. convertToCity returns null if it does not exist
+        const errorCheck = convertCityToLatLon(searchBar.value)
+        if (errorCheck == null) {
+            DisplayController.errorMessage()
+        } else {
+            DisplayController.displayIfNoError()
+            APIController(searchBar.value)
+        }
+    }
+})
 
 APIController("Singapore")
 
